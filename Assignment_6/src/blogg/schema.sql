f57f9dd457b756d9a66a8ebb4b7eb57a163a0247
Ingvild Åsrønning Broen <ingvild.broen@gmail.com>
2018-11-27 20:19:11 +0100

    Assignment 6, initial

---
+++ Assignment_6/src/blogg/schema.sql
@@ -0,0 +1,8 @@
+drop table if exists nyheter;
+create table nyheter (
+  id integer primary key autoincrement,
+  tittel text not null,
+  nyhet text not null,
+  forfatter text not null,
+  dato text not null
+);
